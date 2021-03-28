7195273f3185a02f23f2718e6161e6cc6eb88d93
amandacurry <amandacurry@gmail.com>
2021-03-28 23:26:50 +0100

    First go at website

---
+++ content/blog/davinci.md
@@ -0,0 +1,16 @@
+Title: Episode 27 - What does Leonardo Da Vinci have on AI?
+Date: 2021-03-25
+Category: art
+Slug: davinci-ai
+Summary: Hakuna Matata vive y deja vivir
+EpURL: <iframe title="What does Leonardo da Vinci have on AI?" allowtransparency="true" height="300" width="100%" style="border: none; min-width: min(100%, 430px);" scrolling="no" data-name="pb-iframe-player" src="https://www.podbean.com/player-v2/?i=ctgdi-fecb91-pb&from=pb6admin&download=1&square=1&share=1&download=1&rtl=0&fonts=Arial&skin=1&btn-skin=12&size=300" allowfullscreen=""></iframe>
+
+Is AI a new tool for art or does it take away the human touch? 
+
+This week we look at digital art and art created by AI after the recent news that Christie's Auction House sold the artist Beeple's digital art for $69,346,250. 
+
+<iframe title="What does Leonardo da Vinci have on AI?" allowtransparency="true" height="150" width="100%" style="border: none; min-width: min(100%, 430px);" scrolling="no" data-name="pb-iframe-player" src="https://www.podbean.com/player-v2/?i=ctgdi-fecb91-pb&from=pb6admin&download=1&share=1&download=1&rtl=0&fonts=Arial&skin=1&btn-skin=12"></iframe>
+
+Article mentions: 
+- [Art Net](https://news.artnet.com/art-world/machine-art-versus-human-art-study-1946514)
+- [Christie's](https://www.christies.com/features/Monumental-collage-by-Beeple-is-first-purely-digital-artwork-NFT-to-come-to-auction-11510-7.aspx) 
---
+++ pelicanconf.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Oriana and Amanda'
+SITENAME = "Let's Chat Ethics"
+SITEURL = ''
+
+PATH = 'content'
+THEME = '/Users/amandacurry/pelican-themes/pelican-fh5co-marble'
+TIMEZONE = 'Europe/London'
+
+STATIC_PATHS = [
+    'images',
+    'extra',  # this
+]
+
+PLUGIN_PATHS = ['pelican-plugins', ]
+PLUGINS = ['i18n_subsites', ]
+JINJA_ENVIRONMENT = {
+    'extensions': ['jinja2.ext.i18n'],
+}
+
+LOGO = '/images/logo.png'
+FAVICON = '/images/logo.png'
+DEFAULT_LANG = 'en'
+
+ABOUT = {
+  'image': '/images/orianda.png',
+  'mail': 'letschatethics@gmail.com',
+  # keep it a string if you dont need multiple languages
+  'text': {
+    'en': 'Drop us a message.',
+  },
+  'link': 'contact.html',
+  # the address is also taken for google maps
+  'address': 'London, United Kingdom',
+}
+
+# content paths
+PATH = 'content'
+PAGE_PATHS = ['pages']
+ARTICLE_PATHS = ['blog']
+
+I18N_GETTEXT_LOCALEDIR = '../pelican-fh5co-marble/locale/'
+I18N_GETTEXT_DOMAIN = 'messages'
+I18N_GETTEXT_NEWSTYLE = True
+DEFAULT_DATE_FORMAT = '%a, %d %b %Y'
+I18N_TEMPLATES_LANG = 'en_UK'
+DEFAULT_LANG = 'en'
+LOCALE = 'en_UK'
+# i18n
+
+
+# navigation and homepage options
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_PAGES_ON_HOME = False
+DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_TAGS_ON_MENU = False
+USE_FOLDER_AS_CATEGORY = True
+PAGE_ORDER_BY = 'order'
+
+MENUITEMS = [
+  #('Archive', 'archives.html'),
+  ('Contact', 'contact.html')
+]
+
+DIRECT_TEMPLATES = [
+  'index',
+  'tags',
+  'categories',
+  'authors',
+  'archives',
+  'search', # needed for tipue_search plugin
+  'contact' # needed for the contact form
+]
+
+
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/LetsChatEthics'),
+          ('Instagram', 'https://www.instagram.com/lets.chat.ethics/'),
+          ('LinkedIn', 'https://www.linkedin.com/company/letschatethics/'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
